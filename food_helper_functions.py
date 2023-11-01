8e997b35ea04d898b1ab28787d54004c27f59409
Aleksei <92441134+AlexeyKarz@users.noreply.github.com>
2023-11-01 19:39:30 +0200

    Update food_helper_functions.py

--- food_helper_functions.py
+++ food_helper_functions.py
@@ -108,7 +108,7 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
 
   if classes:
     labels = classes
-  else
+  else:
   labels = np.range(cm.shape[0])
 
   # set the axes
