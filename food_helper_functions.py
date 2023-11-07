12c74f555fe087d93363000e1d9f1d9d1f360f9a
Aleksei <92441134+AlexeyKarz@users.noreply.github.com>
2023-11-07 13:40:29 +0200

    Update food_helper_functions.py

--- food_helper_functions.py
+++ food_helper_functions.py
@@ -140,6 +140,33 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
         if savefig:
           fig.savefig("confusion_matrix.png")
 
+def plot_loss_curves(history):
+  """
+  Returns separate loss curves for training and validation metrics.
+  """ 
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+
+  accuracy = history.history['accuracy']
+  val_accuracy = history.history['val_accuracy']
+
+  epochs = range(len(history.history['loss']))
+
+  # Plot loss
+  plt.plot(epochs, loss, label='training_loss')
+  plt.plot(epochs, val_loss, label='val_loss')
+  plt.title('Loss')
+  plt.xlabel('Epochs')
+  plt.legend()
+
+  # Plot accuracy
+  plt.figure()
+  plt.plot(epochs, accuracy, label='training_accuracy')
+  plt.plot(epochs, val_accuracy, label='val_accuracy')
+  plt.title('Accuracy')
+  plt.xlabel('Epochs')
+  plt.legend();
+
 def compare_historys(original_history, new_history, initial_epochs=5):
     """
     Compares two TensorFlow model History objects.
