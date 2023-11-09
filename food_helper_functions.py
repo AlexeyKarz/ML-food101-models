1987355349ddc896099f0dbda7ac3630437c8091
Aleksei <92441134+AlexeyKarz@users.noreply.github.com>
2023-11-09 14:23:05 +0200

    Update food_helper_functions.py

--- food_helper_functions.py
+++ food_helper_functions.py
@@ -101,48 +101,61 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
   Returns confusing matrix comparing y_true and y_pred
   """
 
+  # Create the confustion matrix
   cm = confusion_matrix(y_true, y_pred)
-  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] # normalize
-  n_classes = cm.shape[0]
+  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] # normalize it
+  n_classes = cm.shape[0] # find the number of classes we're dealing with
+
+  # Plot the figure and make it pretty
   fig, ax = plt.subplots(figsize=figsize)
-  cax = ax.matshow(cm, cmap = plt.cm.Blues) # how correct is (darker - better)
+  cax = ax.matshow(cm, cmap=plt.cm.Blues) # colors will represent how 'correct' a class is, darker == better
   fig.colorbar(cax)
 
+  # Are there a list of classes?
   if classes:
     labels = classes
   else:
-      labels = np.arange(cm.shape[0])
+    labels = np.arange(cm.shape[0])
+  
+  # Label the axes
+  ax.set(title="Confusion Matrix",
+         xlabel="Predicted label",
+         ylabel="True label",
+         xticks=np.arange(n_classes), # create enough axis slots for each class
+         yticks=np.arange(n_classes), 
+         xticklabels=labels, # axes will labeled with class names (if they exist) or ints
+         yticklabels=labels)
     
-      # set the axes
-      ax.set(title = "Confusion matrix",
-             xlabel = "Predicted label",
-             ylabel = "True label",
-             xticks = np.arange(n_classes),
-             yrange = np.arange(n_classes),
-             xticklabels = labels,
-             yticklabels = labels)
-    
-      threshold = (cm.max() + cm.min()) / 2 # set threshold for different colors
-    
-      # plot the text on the cells
-      for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        if norm:
-          plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
-                   horizontalalignment = "center",
-                   color = "white" if cm[i, j] > threshold else "black",
-                   size = text_size)
-        else:
-          plt.text(j, i, f"{cm[i, j]}",
-                   horizontaalignment = "center",
-                   color = "white" if cm[i, j] > threshold else "black",
-                   size = text_size)
-    
-        if savefig:
-          fig.savefig("confusion_matrix.png")
+  # Make x-axis labels appear on bottom
+  ax.xaxis.set_label_position("bottom")
+  ax.xaxis.tick_bottom()
 
+  # Set the threshold for different colors
+  threshold = (cm.max() + cm.min()) / 2.
+
+  # Plot the text on each cell
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    if norm:
+      plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+    else:
+      plt.text(j, i, f"{cm[i, j]}",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+
+  # Save the figure to the current working directory
+  if savefig:
+    fig.savefig("confusion_matrix.png")
+  
 def plot_loss_curves(history):
   """
   Returns separate loss curves for training and validation metrics.
+
+  Args:
+    history: TensorFlow model History object (see: https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/History)
   """ 
   loss = history.history['loss']
   val_loss = history.history['val_loss']
